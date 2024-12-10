164c6c9483cb3aafe3f261e495ddebd456dcc1a4
matthewberes <matthewberes@trentu.ca>
2024-12-10 18:50:16 -0500

    start formatting home page

--- resume-website/src/app/app.routes.ts
+++ resume-website/src/app/app.routes.ts
@@ -1,5 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ResumeComponent } from './resume/resume.component';
 
 export const routes: Routes = [
-    // { path: 'cv', component: ResumeComponent },
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
+    { path: 'cv', component: ResumeComponent },
 ];
