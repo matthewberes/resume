1957a4f83fafd0d4d3c5ad76ec7a365dcf7f26b3
matthewberes <matthewberes@trentu.ca>
2024-12-28 22:32:47 -0500

    pass value to dialog component

--- resume-website/src/app/dialog/dialog.component.ts
+++ resume-website/src/app/dialog/dialog.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-dialog',
@@ -8,5 +9,6 @@ import { Component } from '@angular/core';
   styleUrl: './dialog.component.css'
 })
 export class DialogComponent {
-
+  constructor(@Inject(MAT_DIALOG_DATA) public data: { item: string }) {
+  }
 }
