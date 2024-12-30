a393bc52ac4f5ac3fad9ae6c997b2e29d02a4224
matthewberes <matthewberes@trentu.ca>
2024-12-29 22:54:26 -0500

    add text to dialogs

--- resume-website/src/app/dialog/dialog.component.ts
+++ resume-website/src/app/dialog/dialog.component.ts
@@ -1,14 +1,14 @@
+import { NgIf } from '@angular/common';
 import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-dialog',
   standalone: true,
-  imports: [],
+  imports: [NgIf],
   templateUrl: './dialog.component.html',
   styleUrl: './dialog.component.css'
 })
 export class DialogComponent {
-  constructor(@Inject(MAT_DIALOG_DATA) public data: { item: string }) {
-  }
+  constructor(@Inject(MAT_DIALOG_DATA) public data: { item: string }) { }
 }
